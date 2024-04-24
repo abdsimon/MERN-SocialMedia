a28aa5ae5079b178fd22d0ed128899525dc5c7c6
Simon <diababdulwahed@gmail.com>
2024-04-24 15:55:26 +0200

    changes

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,1 +1,16 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import PostList from "../components/Post/PostList";
+import PostForm from "../components/Post/PostForm";
 
+const Home = () => {
+  return (
+    <div>
+      <h1>Home</h1>
+      <PostForm />
+      <PostList />
+    </div>
+  );
+};
+
+export default Home;
