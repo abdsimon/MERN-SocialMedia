5d110dbc38d43452b50ede5ab307aafd22f2a0aa
Simon <diababdulwahed@gmail.com>
2024-04-24 15:07:12 +0200

    changes

---
+++ server/controllers/postController.js
@@ -0,0 +1,22 @@
+// backend/controllers/postController.js
+const Post = require('../models/Post');
+
+// Get all posts
+exports.getAllPosts = async (req, res) => {
+  // Implement logic to fetch all posts
+};
+
+// Create a new post
+exports.createPost = async (req, res) => {
+
+};
+
+// Update a post
+exports.updatePost = async (req, res) => {
+
+};
+
+// Delete a post
+exports.deletePost = async (req, res) => {
+  
+};
