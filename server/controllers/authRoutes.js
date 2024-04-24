eb917adf45ac6d0f63367c95cc80a6cdbb673129
Simon <diababdulwahed@gmail.com>
2024-04-24 15:02:59 +0200

    changes

---
+++ server/controllers/authRoutes.js
@@ -0,0 +1,16 @@
+const User = require('../models/User');
+
+// Register new user
+exports.register = async (req, res) => {
+
+};
+
+// Login user
+exports.login = async (req, res) => {
+
+};
+
+// Logout user
+exports.logout = async (req, res) => {
+
+};
