82670c4056215c9a8c00c400ba9abc9c6b39601d
Simon <diababdulwahed@gmail.com>
2024-04-25 12:55:45 +0200

    register.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
-import Register from './components/Auth/Register';
+import Register from './pages/Register';
 
 
 const App = () => {
