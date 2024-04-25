6ea81a8f22384cce06845fa791d413303cfcaa0c
Simon <diababdulwahed@gmail.com>
2024-04-25 12:39:52 +0200

    Login.jsx

---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,17 @@
+// frontend/src/components/Login.jsx
+// eslint-disable-next-line no-unused-vars
+import React, { useState } from 'react';
+
+const Login = () => {
+
+  
+
+  return (
+    <div>
+      
+    </div>
+  );
+};
+
+export default Login;
+
