9cb003d7c5cb832a3e6c53b5c8dbadcb907469a1
Simon <diababdulwahed@gmail.com>
2024-04-29 16:50:10 +0200

    changes

--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -4,7 +4,7 @@ const NotFound = () => {
   return (
     <div>
       <h1>404 - Not Found</h1>
-      <p>The page you are looking for does not exist.</p>
+      <p>The page  does not exist.</p>
     </div>
   );
 };
