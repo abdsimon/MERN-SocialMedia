7e43c34ea3c50173eac38c5d601be3ade4b7bcbc
Simon <diababdulwahed@gmail.com>
2024-05-02 21:30:36 +0200

    changes

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -1,23 +1,44 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
-const Profile = () => {
+const UserProfile = () => {
+  // Initialize user state
+  const [user, setUser] = useState(null);
+
   
-  const name = 'John Doe';
-  const email = 'john@example.com';
-  const location = 'New York, USA';
+  useEffect(() => {
+   
+    const timeoutId = setTimeout(() => {
+     
+      const userData = {
+        name: 'John Doe',
+        email: 'john@example.com',
+        location: 'New York,k SA',
+      
+      };
+
+      setUser(userData);
+    }, 1000);
+
+    return () => clearTimeout(timeoutId);
+  }, []);
+
+ 
+  if (!user) {
+    return <p>Loading...</p>;
+  }
 
   return (
     <div>
-      <h2>User Profile</h2>
+      <h1>User Profile</h1>
       <div>
-        <p><strong>Name:</strong> {name}</p>
-        <p><strong>Email:</strong> {email}</p>
-        <p><strong>Location:</strong> {location}</p>
+        <p><strong>Name:</strong> {user.name}</p>
+        <p><strong>Email:</strong> {user.email}</p>
+        <p><strong>Location:</strong> {user.location}</p>
+    
       </div>
     </div>
   );
 };
 
-export default Profile;
-
+export default UserProfile;
